78761bd4a7606bed887c7dc2e969b4005c11740b
WenXiang Ma <2877668629@qq.com>
2021-03-12 11:49:20 +0800

    修改说明

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ __C.DROPOUT                   = 0.5
 __C.MAX_LENGTH                = 128
 __C.BATCH_SIZE                = 8
 __C.EPOCHS                    = 10
-__C.GPU                       = '1'
+__C.GPU                       = '0'
 __C.TRAIN                     = './data/train_p.json'
 __C.DEV                       = './data/dev_p.json'
